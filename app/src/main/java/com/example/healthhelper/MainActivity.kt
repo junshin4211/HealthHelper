aba4ad5a9e94ab930075459c508ff0977b070b3e
MSI\Jay <40843245@gm.nfu.edu.tw>
2024-10-04 20:23:24 +0800

    WTF

--- app/src/main/java/com/example/healthhelper/MainActivity.kt
+++ app/src/main/java/com/example/healthhelper/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "$name!!!!",
+        text = "$name!!!! WTF",
         modifier = modifier
     )
 }
