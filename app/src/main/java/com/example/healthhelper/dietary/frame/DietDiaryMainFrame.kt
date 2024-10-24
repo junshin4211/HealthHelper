c72660dd99270eaa44fb7dca5caabef5c21e7c2f
MSI\Jay <40843245@gm.nfu.edu.tw>
2024-10-24 11:20:58 +0800

    1. Add a functionality -- saving file to external storage in local device with Android App, implemented in `SavingFile` function in `SavingFile.kt`

--- app/src/main/java/com/example/healthhelper/dietary/frame/DietDiaryMainFrame.kt
+++ app/src/main/java/com/example/healthhelper/dietary/frame/DietDiaryMainFrame.kt
@@ -1,11 +1,7 @@
 package com.example.healthhelper.dietary.frame
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.os.Environment
 import android.util.Log
-import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,12 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,12 +24,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -47,14 +37,9 @@ import com.example.healthhelper.dietary.components.bar.appbar.topappbar.QueryTop
 import com.example.healthhelper.dietary.components.button.AddNewDietDiaryItemButton
 import com.example.healthhelper.dietary.components.button.DateButton
 import com.example.healthhelper.dietary.components.button.DownloadButton
-import com.example.healthhelper.dietary.enumclass.DietDiaryScreenEnum
-import com.example.healthhelper.dietary.gson.toJson
-import com.example.healthhelper.dietary.util.file.savingfile.saveEternal
 import com.example.healthhelper.dietary.viewmodel.DiaryViewModel
 import com.example.healthhelper.dietary.viewmodel.MealsOptionViewModel
-import java.io.File
 import java.util.Date
-import kotlin.io.path.Path
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("MutableCollectionMutableState")
@@ -66,12 +51,13 @@ fun DietDiaryMainFrame(
 ) {
     val TAG = "tag_DietDiaryMainFrame"
 
-    val selectedDate = remember { mutableStateOf((Date(System.currentTimeMillis()).toString())) }
-    val selectedMealOptionIndex = remember { mutableIntStateOf(0) }
-
-    val verticalScrollState = rememberScrollState()
+    val context = LocalContext.current
 
     val mealsOptions by mealsOptionViewModel.data.collectAsState()
+
+    val selectedDate = remember { mutableStateOf((Date(System.currentTimeMillis()).toString())) }
+    val selectedMealOptionIndex = remember { mutableIntStateOf(0) }
+    val verticalScrollState = rememberScrollState()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -157,7 +143,9 @@ fun DietDiaryMainFrame(
                                         .weight(0.5f),
                                     contentAlignment = Alignment.Center,
                                 ) {
-                                    DownloadButton()
+                                    DownloadButton(
+                                        context = context,
+                                    )
                                 }
                                 Box(
                                     modifier = Modifier
--- app/src/main/java/com/example/healthhelper/dietary/frame/DietDiaryMealFrame.kt
+++ app/src/main/java/com/example/healthhelper/dietary/frame/DietDiaryMealFrame.kt
@@ -1,13 +1,7 @@
 package com.example.healthhelper.dietary.frame
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.os.Build
-import android.os.Environment
 import android.util.Log
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,41 +11,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.healthhelper.R
 import com.example.healthhelper.dietary.components.bar.appbar.topappbar.DietAppTopBar
+import com.example.healthhelper.dietary.components.button.DownloadButton
 import com.example.healthhelper.dietary.components.card.DietDiaryCards
 import com.example.healthhelper.dietary.components.textfield.outlinedtextfield.SearchTextField
-import com.example.healthhelper.dietary.enumclass.DietDiaryScreenEnum
-import com.example.healthhelper.dietary.gson.toJson
-import com.example.healthhelper.dietary.util.file.savingfile.saveEternal
 import com.example.healthhelper.dietary.viewmodel.DiaryViewModel
-
-import com.example.healthhelper.ui.theme.HealthHelperTheme
-import java.io.File
-import kotlin.io.path.Path
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("MutableCollectionMutableState")
@@ -64,8 +42,6 @@ fun DietDiaryMealFrame(
     val TAG = "tag_DietDiaryMealFrame"
 
     val currentContext = LocalContext.current
-
-    var savingFileFlag by remember { mutableStateOf(false) }
 
     val verticalScrollState = rememberScrollState()
 
@@ -134,20 +110,9 @@ fun DietDiaryMealFrame(
                                         .weight(0.5f),
                                     contentAlignment = Alignment.Center,
                                 ) {
-                                    IconButton(
-                                        colors = IconButtonColors(
-                                            containerColor = Color.Blue,
-                                            contentColor = Color.Blue,
-                                            disabledContainerColor = Color.Gray,
-                                            disabledContentColor = Color.Gray,
-                                        ), onClick = {
-
-                                        }) {
-                                        Image(
-                                            painter = painterResource(R.drawable.download),
-                                            contentDescription = stringResource(R.string.download_icon),
-                                        )
-                                    }
+                                    DownloadButton(
+                                        context = currentContext,
+                                    )
                                 }
                                 Box(
                                     modifier = Modifier
@@ -155,21 +120,7 @@ fun DietDiaryMealFrame(
                                         .weight(0.5f),
                                     contentAlignment = Alignment.Center,
                                 ) {
-                                    IconButton(
-                                        colors = IconButtonColors(
-                                            containerColor = Color.Blue,
-                                            contentColor = Color.Blue,
-                                            disabledContainerColor = Color.Gray,
-                                            disabledContentColor = Color.Gray,
-                                        ), onClick = {
-                                            Log.d(TAG,"add button was clicked.")
-                                            navController.navigate(DietDiaryScreenEnum.AddNewDietDiaryItemFrame.name)
-                                        }) {
-                                        Image(
-                                            imageVector = Icons.Default.Add,
-                                            contentDescription = stringResource(R.string.add_new_item_icon),
-                                        )
-                                    }
+                                   AddNewDietDiaryItemFrame(navController)
                                 }
                             }
                         }
@@ -178,27 +129,4 @@ fun DietDiaryMealFrame(
             }
         }
     )
-
-    Log.d(TAG,"savingFileFlag:$savingFileFlag")
-
-    if (savingFileFlag) {
-        val fileName = "download.txt"
-        val jsonString = diaries.toJson()
-        val folder = File(
-            LocalContext.current.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
-            fileName,
-        )
-        val fullFilePath = Path(folder.toPath().toString(), fileName)
-        val fullFile = fullFilePath.toFile()
-
-        val saveSuccessfully = saveEternal(fullFile, jsonString)
-
-        val currentActivity = LocalContext.current as Activity
-        val toastMessage =
-            if (saveSuccessfully) "Data saved successfully." else "Data saved failed."
-
-        Toast.makeText(currentActivity, toastMessage, Toast.LENGTH_LONG).show()
-
-        savingFileFlag = false
-    }
 }
