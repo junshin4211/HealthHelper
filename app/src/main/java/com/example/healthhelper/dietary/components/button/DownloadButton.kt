00451977cf58682c064d0a76e06bbfee5f5a89b9
MSI\Jay <40843245@gm.nfu.edu.tw>
2024-10-24 11:20:58 +0800

    1. Add a functionality -- saving file to external storage in local device with Android App, implemented in `SavingFile` function in `SavingFile.kt`
    2. Fix the issue the button in `DietDiaryMainFrame.kt` was not shown.

--- app/src/main/java/com/example/healthhelper/dietary/components/button/DownloadButton.kt
+++ app/src/main/java/com/example/healthhelper/dietary/components/button/DownloadButton.kt
@@ -1,8 +1,7 @@
 package com.example.healthhelper.dietary.components.button
 
-import android.app.Activity
+import android.content.Context
 import android.os.Environment
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
@@ -13,19 +12,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.healthhelper.R
 import com.example.healthhelper.dietary.gson.toJson
-import com.example.healthhelper.dietary.util.file.savingfile.saveEternal
+import com.example.healthhelper.dietary.util.file.savingfile.SavingFile
 import com.example.healthhelper.dietary.viewmodel.DiaryViewModel
-import java.io.File
-import kotlin.io.path.Path
 
 @Composable
 fun DownloadButton(
+    context:Context,
     diaryViewModel:DiaryViewModel = viewModel()
 ){
     var savingFileFlag by remember { mutableStateOf(false) }
@@ -48,23 +45,14 @@ fun DownloadButton(
     }
 
     if (savingFileFlag) {
-        val fileName = "download.txt"
         val jsonString = diaries.toJson()
-        val folder = File(
-            LocalContext.current.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
-            fileName,
+
+        SavingFile(
+            context = context,
+            filename = "download.txt",
+            data = jsonString,
+            type = Environment.DIRECTORY_DOWNLOADS,
         )
-        val fullFilePath = Path(folder.toPath().toString(), fileName)
-        val fullFile = fullFilePath.toFile()
-
-        val saveSuccessfully = saveEternal(fullFile, jsonString)
-
-        val currentActivity = LocalContext.current as Activity
-        val toastMessage =
-            if (saveSuccessfully) "Data saved successfully." else "Data saved failed."
-
-        Toast.makeText(currentActivity, toastMessage, Toast.LENGTH_LONG).show()
-
         savingFileFlag = false
     }
 }
