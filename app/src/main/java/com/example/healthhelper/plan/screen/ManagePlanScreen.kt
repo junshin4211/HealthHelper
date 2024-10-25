282f9af4e01b6fcff11233c49eb9a08706610efb
Lucus <junshin4211@gmail.com>
2024-10-25 17:09:44 +0800

    add list

--- app/src/main/java/com/example/healthhelper/plan/screen/ManagePlanScreen.kt
+++ app/src/main/java/com/example/healthhelper/plan/screen/ManagePlanScreen.kt
@@ -1,23 +1,11 @@
 package com.example.healthhelper.plan.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -27,7 +15,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -37,19 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,7 +37,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.healthhelper.R
 import com.example.healthhelper.plan.PlanPage
 import com.example.healthhelper.plan.ui.CustomText
-import com.example.healthhelper.plan.ui.createToggleButton
+import com.example.healthhelper.plan.ui.CreateToggleButton
 import com.example.healthhelper.ui.theme.HealthHelperTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -69,7 +48,7 @@ fun ManagePlan(
 ) {
     val context = LocalContext.current
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-    var activatepannel by remember { mutableStateOf(PlanPage.MyPlan.name) }
+    var activatepannel by remember { mutableStateOf(planpannel) }
     var activatecolor by remember { mutableIntStateOf(R.color.primarycolor) }
     var activatetextcolor by remember { mutableIntStateOf(R.color.white) }
     Scaffold(
@@ -124,16 +103,32 @@ fun ManagePlan(
         ) {
             when(activatepannel){
                 PlanPage.MyPlan.name ->{
-                    //activatecolor = colorResource(id = R.color.primarycolor)
+                    CreateToggleButton(
+                        onLeftClick = {},
+                        onRightClick = {
+                            activatepannel = PlanPage.CompletedPlan.name
+                        },
+                        leftText = PlanPage.MyPlan.getPlanTitle(context),
+                        rightText = PlanPage.CompletedPlan.getPlanTitle(context)
+                    )
+                }
+                PlanPage.CompletedPlan.name ->{
+                    CreateToggleButton(
+                        onLeftClick = {
+                            activatepannel = PlanPage.MyPlan.name
+                        },
+                        onRightClick = {
+                        },
+                        leftButtonColor = R.color.light_gray,
+                        rightButtonColor = R.color.primarycolor,
+                        leftTextColor = R.color.darkgray,
+                        rightTextColor = R.color.white,
+                        leftText = PlanPage.MyPlan.getPlanTitle(context),
+                        rightText = PlanPage.CompletedPlan.getPlanTitle(context)
+                    )
                 }
             }
-            createToggleButton(
-                context,
-                leftText = PlanPage.MyPlan.getPlanTitle(context),
-               rightText = PlanPage.CompletedPlan.getPlanTitle(context)
-            )
 
-            Text(text = planpannel)
         }
 
     }
@@ -144,7 +139,7 @@ fun ManagePlan(
 @Composable
 fun ManageScreenPreview() {
     HealthHelperTheme {
-        //ManagePlan()
+        ManagePlan(planpannel = PlanPage.MyPlan.name)
 
     }
 }
---
+++ app/src/main/java/com/example/healthhelper/plan/ui/CustomList.kt
@@ -0,0 +1,72 @@
+package com.example.healthhelper.plan.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.healthhelper.plan.PlanPage
+import com.example.healthhelper.plan.model.PlanProperty
+
+class CustomList {
+    @Composable
+    fun BookLists(
+        plans: List<PlanProperty>,
+        innerPadding: PaddingValues,
+        onItemClick: (PlanProperty) -> Unit
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            items(plans) { book ->
+                // 用來建立Lists內容物
+                ListItem(
+                    modifier = Modifier.clickable {
+                        onItemClick(book)
+                    },
+                    overlineContent = { Text(text = book.id) },
+                    headlineContent = { Text(book.name) },
+                    supportingContent = { Text(book.price.toString()) },
+                    leadingContent = {
+                        Image(
+                            painter = painterResource(id = book.image),
+                            contentDescription = "book",
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    },
+                    trailingContent = {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "shoppingCart",
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                )
+                HorizontalDivider()
+            }
+        }
+    }
+
+    /**
+     * 載入測試需要資料
+     * @return 多本書資訊
+     */
+    fun fetchBooks(): List<PlanProperty> {
+        return listOf(
+            PlanProperty()
+        )
+    }
+}
