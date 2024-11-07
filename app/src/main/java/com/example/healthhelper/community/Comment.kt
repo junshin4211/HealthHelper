f02ddd35aaa19feca3f8b9efbbf1b315a38a04ae
MSI\Jay <40843245@gm.nfu.edu.tw>
2024-11-07 21:52:02 +0800

    1. add new functionalities about food description (NOT finished yet).
    Finished:
    Add photo.
    Delete photo.
    Add textfield (OutlinedTextField) for description.
    Clear text of description.
    
    NOT finished:
    Save data to Repo.
    Save date to database.

---
+++ app/src/main/java/com/example/healthhelper/community/Comment.kt
@@ -0,0 +1,9 @@
+package com.example.healthhelper.community
+
+class Comment (
+    var userIcon: Int,
+    var userName: String,
+    var reply: String,
+    var index : Int,
+    var commentTime: String
+    )
---
+++ app/src/main/java/com/example/healthhelper/dietary/util/gson/constant/SqlDatePattern.java
@@ -0,0 +1,5 @@
+package com.example.healthhelper.dietary.util.gson.constant;
+
+public class SqlDatePattern {
+	public final static String sqlDatePattern = "yyyy-MM-dd";
+}
