cb2d695d7b6af189463d1a92522169f2200ba475
Yu-Ting <a22627046@gmail.com>
2024-11-11 17:35:48 +0800

    add post picture feature

--- app/src/main/java/com/example/healthhelper/community/CreatePostScreen.kt
+++ app/src/main/java/com/example/healthhelper/community/CreatePostScreen.kt
@@ -1,8 +1,13 @@
 package com.example.healthhelper.community
 
+import android.net.Uri
+import android.util.Base64
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,8 +19,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -28,7 +31,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -39,6 +44,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.healthhelper.R
 import com.example.healthhelper.community.components.CmtNavbarComponent
 import com.example.healthhelper.ui.theme.HealthHelperTheme
@@ -46,12 +52,24 @@ import com.example.healthhelper.ui.theme.HealthHelperTheme
 @Composable
 fun CreatePostScreen(
     navController: NavHostController,
-    postVM: PostVM = viewModel()
+    postVM: PostVM = viewModel(),
 ) {
     var title by remember { mutableStateOf("") }
     var content by remember { mutableStateOf("") }
     var titleError by remember { mutableStateOf(false) }
     var contentError by remember { mutableStateOf(false) }
+    //加入圖片功能
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    /* 呼叫rememberLauncherForActivityResult並搭配PickVisualMedia()
+        以建立可以啟用photo picker的launcher物件。
+        照片挑選完畢會執行onResult並傳來該照片的URI供後續處理 */
+    val pickImageLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri: Uri? ->
+            selectedImageUri = uri
+        }
+    )
 
     Column(
         modifier = Modifier
@@ -147,8 +165,16 @@ fun CreatePostScreen(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
+                //點選跳出相簿處
                 Button(
-                    onClick = { /* 點擊加入圖片的邏輯 */ },
+                    onClick = {
+                        pickImageLauncher.launch(
+                            PickVisualMediaRequest(
+                                // 設定只能挑選圖片
+                                ActivityResultContracts.PickVisualMedia.ImageOnly
+                            )
+                        )
+                    },
                     shape = RoundedCornerShape(size = 9.dp),
                     modifier = Modifier
                         .width(150.dp)
@@ -169,18 +195,29 @@ fun CreatePostScreen(
                         color = colorResource(id = R.color.black_300)
                     )
                 }
-
                 Spacer(modifier = Modifier.width(8.dp))
 
                 Button(
                     onClick = {
-                        // 驗證 title 和 content 是否符合條件
                         titleError = title.isBlank() || title.length > 30
                         contentError = content.isBlank()
 
-                        // 確認 title 和 content 都符合條件時才執行操作
+
+//                        if (!titleError && !contentError) {
+//                            postVM.insertPost(title = title, content = content)
+//                            navController.navigate(CmtScreenEnum.MyPostsScreen.name)
+//                        }
+
                         if (!titleError && !contentError) {
-                            postVM.insertPost(title = title, content = content)
+                            val pictureBytes = selectedImageUri?.let { uri ->
+                                val inputStream = navController.context.contentResolver.openInputStream(uri)
+                                val bytes = inputStream?.readBytes()
+                                inputStream?.close()
+                                bytes  // 直接返回 ByteArray
+                            }
+
+                            // 將 pictureBytes 傳入 insertPost
+                            postVM.insertPost(title, content, pictureBytes)
                             navController.navigate(CmtScreenEnum.MyPostsScreen.name)
                         }
                     },
@@ -198,7 +235,18 @@ fun CreatePostScreen(
                 }
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(40.dp))
+            // 當照片被選取則URI不為null，就將該照片顯示
+            selectedImageUri?.let { uri ->
+                Image(
+                    painter = rememberAsyncImagePainter(uri),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(350.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
@@ -210,5 +258,26 @@ fun CreatePostScreenPreview() {
         CreatePostScreen(rememberNavController())
     }
 }
-
-
+//原Button
+//                Button(
+//                    onClick = { /* 點擊加入圖片的邏輯 */ },
+//                    shape = RoundedCornerShape(size = 9.dp),
+//                    modifier = Modifier
+//                        .width(150.dp)
+//                        .height(52.dp),
+//                    colors = ButtonDefaults.buttonColors(colorResource(id = R.color.gray_100))
+//                ) {
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.baseline_photo_24),
+//                        contentDescription = "Add Image",
+//                        modifier = Modifier,
+//                        colorResource(id = R.color.black_200)
+//                    )
+//                    Spacer(modifier = Modifier.width(8.dp))
+//                    Text(
+//                        text = "加入圖片",
+//                        fontSize = 14.75.sp,
+//                        fontWeight = FontWeight(600),
+//                        color = colorResource(id = R.color.black_300)
+//                    )
+//                }
--- app/src/main/java/com/example/healthhelper/community/components/PostsPreviewComponent.kt
+++ app/src/main/java/com/example/healthhelper/community/components/PostsPreviewComponent.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -19,9 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -84,7 +87,7 @@ fun PostsPreviewComponent(navController: NavHostController, post: Post) {
                         )
                         Spacer(modifier = Modifier.width(10.dp))
                         Text(
-                            text =post.userName,
+                            text = post.userName,
                             fontSize = 18.sp,
                             fontWeight = FontWeight.Bold,
                             color = colorResource(R.color.black_200)
@@ -115,7 +118,8 @@ fun PostsPreviewComponent(navController: NavHostController, post: Post) {
                     val imagePainter = post.picture?.let {
                         runCatching {
                             val decodedImage = Base64.decode(it, Base64.DEFAULT)
-                            val bitmap = BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.size)
+                            val bitmap =
+                                BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.size)
                             BitmapPainter(bitmap.asImageBitmap())
                         }.getOrElse {
                             painterResource(id = R.drawable.postpic)
@@ -126,9 +130,10 @@ fun PostsPreviewComponent(navController: NavHostController, post: Post) {
                         painter = imagePainter,
                         contentDescription = "貼文圖片",
                         modifier = Modifier
-                            .width(189.dp)
-                            .height(107.dp)
-                            .background(colorResource(id = R.color.backgroundcolor))
+                            .size(189.dp, 107.dp)
+                            .clip(RoundedCornerShape(20.dp))
+                            .background(colorResource(id = R.color.backgroundcolor)),
+                        contentScale = ContentScale.Crop
                     )
                 }
             }
